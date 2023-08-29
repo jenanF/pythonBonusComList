d71676e1ac0b635a0a243ead4faf7cbbdf2e66bd
jenanF <108689302+jenanF@users.noreply.github.com>
2023-08-30 02:38:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-'''import random 
-
-chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567!@#$%^&*<?"
-pass_len = int(input("whats your password length: "))
-
-code =  [for i in range(0,pass_len) passcode = random.choice(chars) password += passcode]
-
-print(code)'''
 
 num1 = int(input("enter the first number: "))
 num2 = int(input("enter the secoend number:"))
@@ -13,10 +5,10 @@ operator = input("what would you like to do?")
 
 opr = ["+", "-", "*", "/"]
 
-opration = [num1 + num2 if operator == opr[0] else num1 - num2 if operator == opr[1] else num1 * num2 if operator == opr[2] else num1 / num2 if operator == opr[3] else opr[0]== ("the operation is not valid")   for itm in opr  ]
+opration = [num1 + num2 if operator == opr[0]
+             else num1 - num2 if operator == opr[1]
+               else num1 * num2 if operator == opr[2]
+                 else num1 / num2 if operator == opr[3]
+                   else print("the operation is not valid")]
 
-'''def test(status):
-  if status == "the operation is not valid":
-    print(status)
-  else:'''
 print(opration[0])
