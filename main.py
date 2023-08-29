3a5ebc683f2f8c176bdcd33aa93e10ff21111c38
jenanF <108689302+jenanF@users.noreply.github.com>
2023-08-29 19:52:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import random 
+
+chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567!@#$%^&*<?"
+pass_len = int(input("whats your password length: "))
+
+code =  [for i in range(0,pass_len) passcode = random.choice(chars) password += passcode]
+
+print(code)
